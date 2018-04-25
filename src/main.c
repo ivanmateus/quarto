6989997373226cee144b0a243278d5c64d302a65
ivanmateus <38728334+ivanmateus@users.noreply.github.com>
2018-04-25 20:57:08 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,15 +5,33 @@
 #include "ai.h"
 #include "game.h"
 
-int main(void){
+int main(int argc, char **argv){
 
 	void InitBoard(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
-	void Quarto(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
-
 	int Board[MAXBOARD][MAXBOARD][NOFPOS];
 	InitBoard(Board);
-
-	Quarto(Board);
+	
+	if(argc == 1){
+		void Quarto(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
+		
+		Quarto(Board);
+		return 0;
+	}else{
+		if(strcmp(argv[1], ¨-test¨) == 0){
+			int AIMovewMiniMax(Board[MAXBOARD][MAXBOARD][NOFPOS], int Number, int NumberArray[MAXBOARD*MAXBOARD], int PlayedNumbers[MAXBOARD*MAXBOARD], int NofPlydNbrs)
+			int NumberArray[MAXBOARD*MAXBOARD];
+			int PlayedNumbers[MAXBOARD*MAXBOARD];
+			int Position = -1;
+			int NofPNumbers = 0;
+				
+			NArray(NumberArray);
+			InitArray(PlayedNumbers);
+			
+			
+			Position = AIMovewMiniMax(Board, Number, NumberArray, PlayedNumbers, NofPNumbers);
+				
+		}
+	}
 
 	return 0;
 }
