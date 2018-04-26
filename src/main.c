082a049b5f156f188e369b58d46a28d4e5a95ba8
ivanmateus <38728334+ivanmateus@users.noreply.github.com>
2018-04-25 22:11:08 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,17 +11,20 @@ int main(int argc, char *argv[]){
 	void InitBoard(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
 	int Board[MAXBOARD][MAXBOARD][NOFPOS];
 	InitBoard(Board);
-	
+
 	if(argc == 1){
 		void Quarto(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
-		
+
 		Quarto(Board);
 		return 0;
 	}else{
-		if(strcmp(argv[1], ¨-test¨) == 0){
-			Test(Board);
-			return 0;
+		if(strcmp (argv[1], "-test") == 0){
+		void Test(int Board[MAXBOARD][MAXBOARD][NOFPOS]);
+
+		Test(Board);
+		return 0;
 		}
 	}
 	return 0;
 }
+
